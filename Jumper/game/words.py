bedd2cb6c7620ac184396826de6fc03715a50f88
Christian <vargasc475@gmail.com>
2022-02-09 11:45:35 -0400

    Made changes in the words list and added a hint.

--- Jumper/game/words.py
+++ Jumper/game/words.py
@@ -2,7 +2,7 @@ import random
 
 class Words:
     def __init__(self):
-        self._word_list = ["peru", "venezuela", "chile", "crazil", "colombia", "ecuador", "argentina", "bolivia"]
+        self._word_list = ["peru", "venezuela", "chile", "brazil", "colombia", "ecuador", "argentina", "bolivia"]
         self.word = ""
         
     def make_word(self):
